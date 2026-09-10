1ce7a0ffe2afd5b95bd6ac4bc09892f4d3fbc12a
lex-icl <lexabellanosa.dev@gmail.com>
2026-09-10 20:24:41 +0800

    Urh, tryna reorganize files
    Am doing the projects part of the website, like fixing up the placeholders and such! Imma add like, one placeholder that's intentional for the future, but I'm cleaning up the rest

---
+++ docs/projSites/proj1/script.js
@@ -0,0 +1,21 @@
+const bgMusic = document.getElementById("bgMusic");
+const bgMusicBtn = document.getElementById("musicBtn");
+const bgMusicImg = document.getElementById("musicImg");
+const musicOn = '../../media/nanosong2.png';
+const musicOff = '../../media/nanosong1.png';
+const musicVol = 0.1;
+var musicTime = 0;
+
+bgMusic.volume = musicVol;
+
+bgMusicBtn.addEventListener('click', () => {
+  if (bgMusic.paused) {
+    bgMusic.play();
+
+    bgMusicImg.src = musicOn;
+  } else {
+    bgMusic.pause();
+
+    bgMusicImg.src = musicOff;
+  }
+})
